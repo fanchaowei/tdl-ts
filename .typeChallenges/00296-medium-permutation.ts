be4b11daf28b9e4aa5960a539499faa3c2200309
fanchaowei <597064177@qq.com>
2023-06-12 22:34:45 +0800

    fix: 添加解题注释

--- .typeChallenges/00296-medium-permutation.ts
+++ .typeChallenges/00296-medium-permutation.ts
@@ -35,3 +35,17 @@ type Permutation<T, C = T> = [T] extends [never]
   : T extends C
   ? [T, ...Permutation<Exclude<C, T>>]
   : never
+
+// 我们来做一下分析
+// 首先传入 A | B | C
+// 通过 extends 判定最后走到 [T, ...Permutation<Exclude<C, T>>] 这步，带入 A | B | C 则变为如下：
+// [A, ...Permutation<Exclude<A | B | C, A>>] | [B, ...Permutation<Exclude<A | B | C, B>>] | [C, ...Permutation<Exclude<A | B | C, C>>]
+// 我们拿 [A, ...Permutation<Exclude<A | B | C, A>>] 继续分析
+// 处理了 Exclude 后，就是 [A, ...Permutation<Exclude<B | C>>]
+// 然后我们继续走入下一步，得到 [A, ...[B, ...Permutation<Exclude<B | C, B>>]] | [A. ...[C, ...Permutation<Exclude<B | C, C>>]]
+// 我们继续取第一个分析
+// 结算了 Exclude 之后，就是 [A, ...[B, ...Permutation<C>]]
+// 我们继续处理里面的 Permutation<C>
+// 得到 [A, ...[B, ...[C, ...Permutation<Exclude<C, C>>]]]，即 [A, ...[B, ...[C, ...Permutation<never>]]]
+// 这时候，我们的 [T] extends [never] 就起作用了
+// 最终得到 [A, ...[B, ...[C, ...[]]]] ，即 [A, B, C]
