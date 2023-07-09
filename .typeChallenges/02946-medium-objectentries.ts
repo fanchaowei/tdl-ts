9bee31055bcd3ab75e9f9955845d1b1070e66c41
fanchaowei <597064177@qq.com>
2023-07-09 20:19:03 +0800

    feat: finish 2946

---
+++ .typeChallenges/02946-medium-objectentries.ts
@@ -0,0 +1,27 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface Model {
+  name: string
+  age: number
+  locations: string[] | null
+}
+
+type ModelEntries =
+  | ['name', string]
+  | ['age', number]
+  | ['locations', string[] | null]
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ['key', undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>
+]
+
+// ============= Your Code Here =============
+type GetKeyArr<T> = [keyof T]
+
+type ObjectEntries<T> = [...GetKeyArr<T>, T[...GetKeyArr<T>]]
+
+type Demo = GetKeyArr<Partial<Model>>
